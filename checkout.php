196cfe249841a1d431277ba45349910f8eec89e3
DasariHarsha <85623307+DasariHarsha@users.noreply.github.com>
2023-12-06 02:42:15 +0530

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -1,10 +1,13 @@
+<?php
+// Start the session
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <?php
 include("connection/connect.php");
 include_once 'product-action.php';
 error_reporting(0);
-session_start();
 function debug_to_console($data) {
     $output = $data;
     if (is_array($output))
