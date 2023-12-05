b2d57da910733de31526df8f560627c96c829b33
DasariHarsha <85623307+DasariHarsha@users.noreply.github.com>
2023-12-06 04:01:07 +0530

    Update delete_orders.php

--- delete_orders.php
+++ delete_orders.php
@@ -1,4 +1,3 @@
-
 <?php
 include("connection/connect.php"); //connection to db
 error_reporting(0);
@@ -9,4 +8,4 @@ session_start();
 mysqli_query($db,"DELETE FROM users_orders WHERE o_id = '".$_GET['order_del']."'"); // deletig records on the bases of ID
 header("location:your_orders.php");  //once deleted success redireted back to current page
 
-
+?>
