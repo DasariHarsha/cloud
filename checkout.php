67ad36b6961bbc794f419995314f683e827e1d67
DasariHarsha <85623307+DasariHarsha@users.noreply.github.com>
2023-12-06 02:00:09 +0530

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -1,29 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <?php
 include("connection/connect.php");
 include_once 'product-action.php';
 error_reporting(0);
 session_start();
-
-
-foreach ($_SESSION["cart_item"] as $item)
+if(empty($_SESSION["user_id"]))
 {
-											
-$item_total += ($item["price"]*$item["quantity"]);
-if($_POST['submit'])
-{
-
-$SQL="insert into users_orders(u_id,title,quantity,price) values('".$_SESSION["user_id"]."','".$item["title"]."','".$item["quantity"]."','".$item["price"]."')";
-
-	mysqli_query($db,$SQL);
-	
-	$success = "Thankyou! Your Order Placed Successfully!";
-}										  
-													
+	header('location:login.php');
 }
+else{
 
+										  
+												foreach ($_SESSION["cart_item"] as $item)
+												{
+											
+												$item_total += ($item["price"]*$item["quantity"]);
+												
+													if($_POST['submit'])
+													{
+						
+													$SQL="insert into users_orders(u_id,title,quantity,price) values('".$_SESSION["user_id"]."','".$item["title"]."','".$item["quantity"]."','".$item["price"]."')";
+						
+														mysqli_query($db,$SQL);
+														
+														$success = "Thankyou! Your Order Placed Successfully!";
+
+														
+														
+													}
+												}
 ?>
 
 
